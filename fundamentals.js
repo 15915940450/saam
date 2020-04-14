5e136b469a22c64ca402501fa5c92038c630095c
15915940450 <15915940450@139.com>
2020-04-14 15:10:14 +0800

    filter

---
+++ fundamentals.js
@@ -0,0 +1,89 @@
+import * as THREE from './__three.js-master/build/three.module.js';
+console.log(THREE); //THREE={...}
+/*
+1.函数默认值
+2.Promise
+3.yield
+4.Array.from(new Set(array));
+5.class SkinnedMesh extends THREE.Mesh {}
+6.Template Strings
+7.Modules
+*/
+
+
+/*
+CathayPacificAirways国泰航空公司1
+HSBCHoldings汇丰控股2
+SwirePacific太古集团3
+ThePeninsulaGroup半岛集团4
+SunHungKaiProperties新鸿基地产5
+Shangri-LaAsia香格里拉（亚洲）6
+MandarinOriental香港文华东方7
+EspritHoldings思捷环球8
+HangSengBank恒生银行9
+CheungKong(Holdings)长江实业(集团)有限公司10
+CLPHoldings中电控股11
+Li&Fung利丰有限公司12
+HutchisonWhampoa和记黄埔企业有限公司13
+MTRCorp.香港地铁有限公司14
+DairyFarmInternationalHoldings牛奶国际控股有限公司15
+HongKong&ChinaGas(Towngas)香港中华煤气有限公司16
+KowloonMotorBus九龙巴士股份有限公司17
+GiordanoInternational左丹奴国际18
+TelevisionBroadcasts电视广播有限公司19
+JardineMathesonHoldings怡和控股20
+WharfHoldings九龙仓控股21
+HongkongElectricHoldings香港电灯集团22
+BankofEastAsia东亚银行23
+KerryProperties嘉里建设有限公司24
+PCCW电讯盈科25
+KCR(Kowloon-CantonRailwayCorp.)九广铁路公司26
+HopewellHoldings合和实业有限公司27
+HongKongExchanges&Clearing香港交易所28
+ChinaMobile(HongKong)中国移动（香港）29
+NewWorldDevelopment新世界发展有限公司30
+CITICPacific中信泰富有限公司31
+JohnsonElectricHoldings德昌电机控股有限公司32
+HendersonLandDevelopment恒基兆业地产有限公司33
+HangLungGroup恒隆集团34
+BOCHongKong(Holdings)中银香港（控股）35
+YueYuenIndustrial(Holdings)裕元集团36
+CoscoPacific中远太平洋有限公司37
+LenovoGroup联想集团38
+ChinaUnicom中国联合通信有限公司39
+CNOOC中国海洋石油总公司40
+*/
+
+
+
+
+/*Thilina(房学聪)
+
+審批通過
+
+審批編號201912311112000126966
+所在部門前端开发组
+所属部门应用开发部
+入职时间2018-01-01
+工作岗位前端开发工程师
+请根据本季度的工作内容和对照岗位职责，认真撰写总结、述职报告↓
+本季度主要工作任务及内容
++ 开发了中控返修平台
++ 实现远程命令短信功能
++ 实现代缴押金功能
++ 实现专有网络的设置
++ 管理更多套餐功能
++ 查询硬件SN
++ 换电柜批量设定更多选项
++ 管理后台添加账号申请和重置密码功能
+本季度工作业绩
+1. 完成基本的开发任务。
+2. 维护好线上的e换电后台管理系统，尽量保证不在正式环境中产生缺陷，能及时迅速解决前端问题。
+3. 帮助身边同事共同解决较复杂的问题。
+4. 与同事关系良好，责任或欢乐同在，热爱公司，热爱工作，适时加班。
+
+迅速完成开发工作，不拖累项目进度
+
+*/
+
+
