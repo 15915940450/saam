a212ac2b116ee488ea2128b9f791ba0df594dac8
15915940450 <15915940450@139.com>
2020-04-15 14:29:04 +0800

    fundamentals

--- fundamentals.js
+++ fundamentals.js
@@ -1,9 +1,42 @@
 import * as THREE from './__three.js-master/build/three.module.js';
 console.log(THREE); //THREE={...}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 1.函数默认值
 2.Promise
-3.yield
+3.yield(https://es6.ruanyifeng.com/#docs/async)
 4.Array.from(new Set(array));
 5.class SkinnedMesh extends THREE.Mesh {}
 6.Template Strings
@@ -22,6 +55,7 @@ MandarinOriental香港文华东方7
 EspritHoldings思捷环球8
 HangSengBank恒生银行9
 CheungKong(Holdings)长江实业(集团)有限公司10
+
 CLPHoldings中电控股11
 Li&Fung利丰有限公司12
 HutchisonWhampoa和记黄埔企业有限公司13
@@ -57,33 +91,5 @@ CNOOC中国海洋石油总公司40
 
 
 
-/*Thilina(房学聪)
-
-審批通過
-
-審批編號201912311112000126966
-所在部門前端开发组
-所属部门应用开发部
-入职时间2018-01-01
-工作岗位前端开发工程师
-请根据本季度的工作内容和对照岗位职责，认真撰写总结、述职报告↓
-本季度主要工作任务及内容
-+ 开发了中控返修平台
-+ 实现远程命令短信功能
-+ 实现代缴押金功能
-+ 实现专有网络的设置
-+ 管理更多套餐功能
-+ 查询硬件SN
-+ 换电柜批量设定更多选项
-+ 管理后台添加账号申请和重置密码功能
-本季度工作业绩
-1. 完成基本的开发任务。
-2. 维护好线上的e换电后台管理系统，尽量保证不在正式环境中产生缺陷，能及时迅速解决前端问题。
-3. 帮助身边同事共同解决较复杂的问题。
-4. 与同事关系良好，责任或欢乐同在，热爱公司，热爱工作，适时加班。
-
-迅速完成开发工作，不拖累项目进度
-
-*/
 
 
