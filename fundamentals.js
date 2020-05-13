3f83a1c63af4cdfff5a203638fcdef68eafedbde
15915940450 <15915940450@139.com>
2020-05-13 15:07:23 +0800

    set up

--- fundamentals.js
+++ fundamentals.js
@@ -1,131 +1,11 @@
 import * as THREE from './__three.js-master/build/three.module.js';
-// console.log(THREE); //THREE={...}
-
-
-/*
-1.函数默认值
-2.Promise
-3.yield(https://es6.ruanyifeng.com/#docs/async)
-4.Array.from(new Set(array));
-5.class SkinnedMesh extends THREE.Mesh {}
-6.Template Strings
-7.Modules
-*/
-
-
 //===============================================
 // https://threejsfundamentals.org/threejs/lessons/threejs-scenegraph.html
-/*function main2(){
-  const canvasCopy=document.querySelector('#c');
-  const renderer=new THREE.WebGLRenderer({canvasCopy});
-
-
-
-
-  const fov=40;
-  const aspect=2;
-  const near=0.1;
-  const far=1000;
-  const camera=new THREE.PerspectiveCamera(fov,aspect,near,far);
-  camera.position.set(0,50,0);
-  camera.up.set(0,0,1);
-  camera.lookAt(0,0,0);
-
-  const scene=new THREE.Scene();
-
-  {
-    const color=0xFFFFFF;
-    const intensity=3;
-    const light=new THREE.PointLight(color,intensity);
-    scene.add(light);
-  }
-
-  const objects=[];
-
-  const radius=1;
-  const widthSegments=6;
-  const heightSegments=6;
-  const sphereGeometry=new THREE.SphereBufferGeometry(radius,widthSegments,heightSegments);
-
-  const solarSystem=new THREE.Object3D();
-  scene.add(solarSystem);
-  objects.push(solarSystem);
-
-  const sunMaterial=new THREE.MeshPhongMaterial({
-    emissive:0xFF0000
-  });
-  const sunMesh=new THREE.Mesh(sphereGeometry,sunMaterial);
-  sunMesh.scale.set(5,5,5);
-
-  solarSystem.add(sunMesh);
-  objects.push(sunMesh);
-
-
-  const earthOrbit=new THREE.Object3D();
-  earthOrbit.position.x=10;
-  solarSystem.add(earthOrbit);
-  objects.push(earthOrbit);
-
-  const earthMaterial = new THREE.MeshPhongMaterial({color: 0x2233FF, emissive: 0x112244});
-  const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
-  earthOrbit.add(earthMesh);
-  objects.push(earthMesh);
-
-  const moonOrbit=new THREE.Object3D();
-  moonOrbit.position.x=2;
-  earthOrbit.add(moonOrbit);
-
-
-  const moonMaterial=new THREE.MeshPhongMaterial({
-    color:0x888888,
-    emissive:0x222222
-  });
-  const moonMesh=new THREE.Mesh(sphereGeometry,moonMaterial);
-  moonMesh.scale.set(0.5,0.5,0.5);
-  moonOrbit.add(moonMesh);
-  objects.push(moonMesh);
-
-  objects.forEach((node) => {
-    const axes=new THREE.AxesHelper();
-    axes.material.depthTest=false;
-    axes.renderOrder=1;
-    node.add(axes);
-  });
-
-
-
-  function resizeRendererToDisplaySize(renderer){
-    const canvas=renderer.domElement;
-    const width=canvas.clientWidth;
-    const height=canvas.clientHeight;
-    const needResize=canvas.width!==width || canvas.height!==height;
-    if(needResize){
-      renderer.setSize(width,height,false);
-    }
-
-  }
-  function render(time){
-    time*=0.001;
-    if(resizeRendererToDisplaySize(renderer)){
-      const canvas=renderer.domElement;
-      camera.aspect=canvas.clientWidth/canvas.clientHeight;
-      camera.updateProjectionMatrix();
-    }
-    objects.forEach((obj) => {
-      obj.rotation.y=time;
-    });
-    renderer.render(scene,camera);
-
-    requestAnimationFrame(render);
-  }
-  requestAnimationFrame(render);
-}
-main2();
-*/
-
 function main() {
   const canvas = document.querySelector('#c_copy');
-  const renderer = new THREE.WebGLRenderer({canvas});
+  const renderer = new THREE.WebGLRenderer({
+    canvas:canvas
+  });
 
   const fov = 40;
   const aspect = 2;  // the canvas default
@@ -148,23 +28,33 @@ function main() {
   const objects = [];
 
   const radius = 1;
-  const widthSegments = 6;
-  const heightSegments = 6;
+  const widthSegments = 60;
+  const heightSegments = 60;
   const sphereGeometry = new THREE.SphereBufferGeometry(
       radius, widthSegments, heightSegments);
 
   const solarSystem = new THREE.Object3D();
+
+  
+
   scene.add(solarSystem);
   objects.push(solarSystem);
 
-  const sunMaterial = new THREE.MeshPhongMaterial({emissive: 0xFFFF00});
+  const sunMaterial = new THREE.MeshPhongMaterial({emissive: 0xec4783});
   const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
-  sunMesh.scale.set(5, 5, 5);
+  sunMesh.scale.set(8, 8, 8);
   solarSystem.add(sunMesh);
+
+  var axes=new THREE.AxesHelper(2);
+  axes.material.depthTest=false;
+  sunMesh.add(axes);
+
+
+  console.log(axes);
   objects.push(sunMesh);
 
   const earthOrbit = new THREE.Object3D();
-  earthOrbit.position.x = 10;
+  earthOrbit.position.x = 13;
   solarSystem.add(earthOrbit);
   objects.push(earthOrbit);
 
@@ -179,16 +69,30 @@ function main() {
 
   const moonMaterial = new THREE.MeshPhongMaterial({color: 0x888888, emissive: 0x222222});
   const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial);
-  moonMesh.scale.set(.5, .5, .5);
+  moonMesh.scale.set(.4, .4, .4);
   moonOrbit.add(moonMesh);
   objects.push(moonMesh);
 
+
+
+  var gridHelper=new THREE.GridHelper();
+  earthOrbit.add(gridHelper);
+  // var gridHelper2=new THREE.GridHelper();
+  // moonOrbit.add(gridHelper2);
+
   // add an AxesHelper to each node
   objects.forEach((node) => {
-    const axes = new THREE.AxesHelper();
+    console.log(node.type); //Object3D,Mesh
+    /*if(node.type!=='Object3D'){
+      var axes=new THREE.AxesHelper(10);
+      axes.material.depthTest=false;
+      axes.renderOrder=1;
+      node.add(axes);
+    }*/
+    /*const axes = new THREE.AxesHelper(19);
     axes.material.depthTest = false;
     axes.renderOrder = 1;
-    node.add(axes);
+    node.add(axes);*/
   });
 
   function resizeRendererToDisplaySize(renderer) {
@@ -203,7 +107,7 @@ function main() {
   }
 
   function render(time) {
-    time *= 0.001;
+    time *= 0.0001;
 
     if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
@@ -223,4 +127,9 @@ function main() {
   requestAnimationFrame(render);
 }
 
+//setup
 main();
+
+//rotation
+//position
+//scale
