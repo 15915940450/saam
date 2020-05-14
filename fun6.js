6763daf9a6b2ec3a497118b9bcc6b91747db9c2f
15915940450 <15915940450@139.com>
2020-05-14 15:46:30 +0800

    phong

--- fun6.js
+++ fun6.js
@@ -9,30 +9,41 @@ class Fun6 {
     this.createAcamera();
     this.createScene();
     this.mesh();
+    this.createLight();
 
     this.render();
-    console.log(this);
+    // console.log(this);
   }
 
-  //渲染
+  //===渲染
   render(){
+    var f=this;
     var renderer=new THREE.WebGLRenderer({
       canvas:this.canvas
     });
-    renderer.render(this.scene,this.camera);
-    // console.log(renderer);
+    
+    var rafCallback=function(time){
+      time=time/1000;
+
+      f.mesh.rotation.y=time*2;
+      f.mesh.rotation.x=time/2;
+
+      renderer.render(f.scene,f.camera);
+      window.requestAnimationFrame(rafCallback);
+    };
+    window.requestAnimationFrame(rafCallback);
   }
 
   //相机
   createAcamera(){
     var fov=75;
-    var aspect=2;
+    var aspect=9/8; //canvas width / height
     var near=0.1;
     var far=5;
     //摄像机默认指向Z轴负方向，上方向朝向Y轴正方向。
     this.camera=new THREE.PerspectiveCamera(fov,aspect,near,far);
 
-    this.camera.position.z=2;
+    this.camera.position.z=3;
   }
 
   //场景
@@ -44,11 +55,11 @@ class Fun6 {
   //物体
   mesh(){
     var geometry=new THREE.BoxGeometry(1,1,1);
-    var material=new THREE.MeshBasicMaterial({
-      color:0xff00ff
+    var material=new THREE.MeshPhongMaterial({
+      color:0xec4783
     });
     this.mesh=new THREE.Mesh(geometry,material);
-    console.log(this.mesh);
+    // console.log(this.mesh);
 
 
 
@@ -56,11 +67,20 @@ class Fun6 {
     this.scene.add(this.mesh);
   }
 
+  //灯光
+  createLight(){
+    var intensity=2;
+    this.light=new THREE.DirectionalLight(0xffffff,intensity);
+    this.light.position.set(-1,2,4);
+
+    this.scene.add(this.light);
+  }
+
 
 
 }
 
 
-var obj=new Fun6();
-obj.init();
+window.obj=new Fun6();
+window.obj.init();
 
