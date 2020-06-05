8cc368ef3be7b1ce585ae569d4da48abf696fe3d
15915940450 <15915940450@139.com>
2020-06-05 15:28:09 +0800

    MS

--- light.js
+++ light.js
@@ -2,74 +2,77 @@
 功能1：计算多个矩阵乘积
 功能2：求n阶行列式
 */
-//矩阵 matrix Amn m*n m行n列
-/*
-当矩阵A的列数（column）等于矩阵B的行数（row）时，A与B可以相乘。
-*/
-/*
-let Amn=[];
-let Bnp=[];
-let Cpq=[];
-*/
 
-let Amn=[[1,0,2],[-1,3,1]];
-let Bnp=[[3,1],[2,1],[1,0]];
-let Cpq=[[1,2,3,4,5],[5,6,7,8,1]];
-
-let multiplyAB=function(Amn,Bnp){
-  let AmnBnp=[],i,j,k;
-  // 求出m,n,p
-  const m=Amn.length;
-  const n=Amn[0].length;
-  const n2=Bnp.length;
-  const p=Bnp[0].length;
-
-  //可乘性
-  if(n!==n2){
-    var msg='警告：两个矩阵不可相乘';
-    // console.error(msg);
-    return msg;
+class Matrix {
+  /*
+  矩阵 matrix Amn m*n m行n列
+  当矩阵A的列数（column）等于矩阵B的行数（row）时，A与B可以相乘。
+  */
+  constructor(){
+    this.Amn=[[1,0,2],[-1,3,1]];
+    this.Bnp=[[3,1],[2,1],[1,0]];
+    this.Cpq=[[1,2,3,4,5],[5,6,7,8,1]];
   }
 
-  let formula=function(i,j){
-    let Cij=0;
-    for(k=0;k<n;k++){
-      Cij+=Amn[i][k]*Bnp[k][j];
+  init(){
+    let result=this.multiply(this.Amn,this.Bnp,this.Cpq);  //ˈmʌltɪplaɪ
+    console.log(JSON.stringify(result));
+    /*
+    [
+      [10,16,22,28,26],
+      [14,20,26,32,22]
+    ]
+    */
+  }
+
+  multiplyAB(Amn,Bnp){
+    let AmnBnp=[],i,j,k;
+    // 求出m,n,p
+    const m=Amn.length;
+    const n=Amn[0].length;
+    const n2=Bnp.length;
+    const p=Bnp[0].length;
+
+    //可乘性
+    if(n!==n2){
+      var msg='警告：两个矩阵不可相乘';
+      // console.error(msg);
+      return msg;
     }
-    return Cij;
-  };
 
-  for(i=0;i<m;i++){
-    AmnBnp.push([]);
-    for(j=0;j<p;j++){
-      //确定值(公式)
-      AmnBnp[i][j]=formula(i,j);
+    let formula=function(i,j){
+      let Cij=0;
+      for(k=0;k<n;k++){
+        Cij+=Amn[i][k]*Bnp[k][j];
+      }
+      return Cij;
+    };
+
+    for(i=0;i<m;i++){
+      AmnBnp.push([]);
+      for(j=0;j<p;j++){
+        //确定值(公式)
+        AmnBnp[i][j]=formula(i,j);
+      }
     }
+
+    return (AmnBnp);
   }
-
-  return (AmnBnp);
-};
-let multiply=function(...param){
-  var result=[];
-  for(var i=0;i<param.length-1;i++){
-    result=multiplyAB(param[i],param[i+1]);
-    param[i+1]=result;
+  multiply(...param){
+    var result=[];
+    for(var i=0;i<param.length-1;i++){
+      result=this.multiplyAB(param[i],param[i+1]);
+      param[i+1]=result;
+    }
+    
+    return result;
   }
-  
-  return result;
-};
-let result=multiply(Amn,Bnp,Cpq);  //ˈmʌltɪplaɪ
-console.log(result);
+} //class Matrix
+
+var matrix=new Matrix();
+matrix.init();
 
 
-//关键字：全排列, 字典序,lexicographical order
-/*
-[3,2,-1,4],
-[1,-3/2,5/2,1/2],
-[1,0,-2,3],
-[5,4,1,2]
--37
-*/
 class DETerminant{
   constructor(){
     this.D=[
@@ -79,7 +82,7 @@ class DETerminant{
       [5,4,1,2]
     ];
     this.FullPermutation=[];
-    this.result=0;  //等于所有取自不同行不同列的n个元素的乘积的代数和
+    this.result=0;  //等于所有取自不同行不同列的n个元素的乘积的代数和 (-37)
   }
   init(){
     var f=this,i;
@@ -118,14 +121,14 @@ class DETerminant{
       return (v);
     });
     this.DET();
+
+    console.log(this.result);
   }
   DET(){
     //所有n级排列求和
     for(var i=0;i<this.FullPermutation.length;i++){
       this.result+=this.FullPermutation[i].hong;
     }
-      
-    console.log(this.result);
   }
   //求一个数组的逆序数
   InversePairs(arr = []) {
@@ -195,6 +198,72 @@ class DETerminant{
   }
 }   //class
 
-var obj=new DETerminant();
-obj.init();
-console.log(obj);
+var det=new DETerminant();
+det.init();
+// https://www.jianshu.com/p/33cffa1ce613 简书 [图解] 归并排序
+
+// console.log('逆序数',det.InversePairs([5,4,7,9,3,8,2,1]));
+
+
+//https://www.bilibili.com/video/BV1Ax411U7Xx?from=search&seid=9095028233196157434
+
+var MERGE=function(arrSortedLeft,arrSortedRight){
+  var arrSorted=[],tau=0;
+  var h=0,f=0,g=0;
+  
+  while(f<arrSortedLeft.length && g<arrSortedRight.length){
+    if(arrSortedLeft[f]>arrSortedRight[g]){
+      //逆序
+      tau+=arrSortedLeft.length-f;
+      arrSorted[h]=arrSortedRight[g];
+      h++;
+      g++;
+    }else{
+      arrSorted[h]=arrSortedLeft[f];
+      h++;
+      f++;
+    }
+  }
+
+  while(f<arrSortedLeft.length){
+    arrSorted[h]=arrSortedLeft[f];
+    h++;
+    f++;
+  }
+
+  while(g<arrSortedRight.length){
+    arrSorted[h]=arrSortedRight[g];
+    h++;
+    g++;
+  }
+
+  return {
+    arrSorted:arrSorted,
+    tau:tau
+  };
+};
+var arrNeedToSort=[5,4,7,9,3,8,2];
+console.log('逆序数',det.InversePairs(arrNeedToSort));
+
+var MERGE_SORT=function(arrNeedToSort){
+  var arrLeft,arrRight,arrSorted;
+  if(arrNeedToSort.length===1){
+    return (arrNeedToSort);
+  }
+
+
+  var Mr=arrNeedToSort.length/2;
+  arrLeft=arrNeedToSort.slice(0,Mr);
+  arrRight=arrNeedToSort.slice(Mr);
+
+
+  var arrSortedLeft=MERGE_SORT(arrLeft);
+  var arrSortedRight=MERGE_SORT(arrRight);
+  var obj=MERGE(arrSortedLeft,arrSortedRight);
+  console.log(obj);
+  arrSorted=obj.arrSorted;
+  return arrSorted;
+};
+var arrSorted=MERGE_SORT(arrNeedToSort);
+console.log(arrSorted);
+
