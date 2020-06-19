aaaf7f5ed41715b8560be77d41b20e444c370c7d
15915940450 <15915940450@139.com>
2020-06-19 14:46:52 +0800

    Hex or hsl?

--- picking.js
+++ picking.js
@@ -5,32 +5,27 @@ import {GUI} from './__3/dat.gui.module.js';
 window.__3=THREE;
 
 class PickHelper {
-  constructor() {
-    this.raycaster = new THREE.Raycaster();
-    this.pickedObject = null;
-    this.pickedObjectSavedColor = 0;
+  constructor(){
+    this.raycaster=new THREE.Raycaster();
+    this.pickedObject=null;
+    this.pickedObjectSavedColor=0;
   }
-  pick(normalizedPosition, scene, camera, time) {
-    // console.log(normalizedPosition);
-    // restore the color if there is a picked object
-    if (this.pickedObject) {
+  pick(normalizedPosition,scene,camera,time){
+    //设定上一次pick的物件
+    if(this.pickedObject){
       this.pickedObject.material.color.setHex(this.pickedObjectSavedColor);
-      this.pickedObject = undefined;
+      this.pickedObject=undefined;
     }
 
-    // cast a ray through the frustum
-    this.raycaster.setFromCamera(normalizedPosition, camera);
-    // get the list of objects the ray intersected
-    // const intersectedObjects = this.raycaster.intersectObjects(scene.children[0].children);
-    var rootObject3D=scene.getObjectByName('rootObject3D');
-    const intersectedObjects = this.raycaster.intersectObjects(rootObject3D.children);
-    if (intersectedObjects.length) {
-      // pick the first object. It's the closest one
-      this.pickedObject = intersectedObjects[0].object;
-      // save its color
-      this.pickedObjectSavedColor = this.pickedObject.material.color.getHex();
-      // set its color color to flashing red/yellow
-      this.pickedObject.material.color.setHex((time * 8) % 2 > 1 ? 0xFFFF00 : 0xFF0000);
+    this.raycaster.setFromCamera(normalizedPosition,camera);
+    var objects=scene.children;
+    var intersectedObjects=this.raycaster.intersectObjects(objects);
+    // console.log(objects,intersectedObjects);
+
+    if(intersectedObjects.length){
+      this.pickedObject=intersectedObjects[0].object;
+      this.pickedObjectSavedColor=this.pickedObject.material.color.getHex();
+      this.pickedObject.material.color.setHex((time*5)%2>1?0xFFFFFF:0x000000);
     }
   }
 }
@@ -38,6 +33,7 @@ class PickHelper {
 class O_xyz {
   constructor(){
     this.canvas=document.querySelector('#c');
+    this.cubeNums=1e2;
     this.pickPosition={
       x:0,
       y:0
@@ -51,9 +47,9 @@ class O_xyz {
     this.addLight();
     this.gui();
 
-    this.render();
+    this.addPickEvent();
 
-    this.pickEvent();
+    this.render();
     // console.log(this);
   }
 
@@ -65,7 +61,7 @@ class O_xyz {
     });
 
     //The first thing we need to do is turn on shadows in the renderer.
-    renderer.shadowMap.enabled=true;
+    // renderer.shadowMap.enabled=true;
 
     
     //raf
@@ -82,7 +78,7 @@ class O_xyz {
         f.camera.updateProjectionMatrix();
       }
 
-      window.pickHelper.pick(f.pickPosition,f.scene,f.camera,time);
+      pickHelper.pick(f.pickPosition,f.scene,f.camera,time);
 
       renderer.render(f.scene,f.camera);
 
@@ -113,16 +109,15 @@ class O_xyz {
     let fov=45;
     let aspect=2; //默认是2
     let near=0.1;
-    let far=100;
+    let far=1000;
     //摄像机默认指向Z轴负方向，上方向朝向Y轴正方向。
     this.camera=new THREE.PerspectiveCamera(fov,aspect,near,far);
 
-    this.camera.position.set(0,0,30);
+    this.camera.position.set(5,10,30);
     // this.camera.up.set(0,0,1);
     // this.camera.lookAt(0,0,0);
-
-
-
+    this.cameraPole=new THREE.Object3D();
+    this.cameraPole.add(this.camera);
   }
   //环绕控制
   createControls(){
@@ -136,112 +131,44 @@ class O_xyz {
     this.scene=new THREE.Scene();
     this.scene.background=new THREE.Color(0x333333);
 
-    /*this.cameraPole=new THREE.Object3D();
     this.scene.add(this.cameraPole);
-    this.cameraPole.add(this.camera);*/
   }
 
 
   //物件
   addMesh(){
-    let f=this;
-
-    this.mesh=[];
-
-    //rootObject3D
-    let rootObject3D=new THREE.Object3D();
-    rootObject3D.name='rootObject3D';
-    this.mesh.push(rootObject3D);
-
-    let plane=this.createPlane();
-    rootObject3D.add(plane);
-    this.mesh.push(plane);
-
-    let cube=this.createCube();
-    rootObject3D.add(cube);
-    this.mesh.push(cube);
-
-    let sphere=this.createSphere();
-    rootObject3D.add(sphere);
-    this.mesh.push(sphere);
-
-    //添加辅助axes,grid
-    this.mesh.forEach(function(mesh,i){
-      // console.log(mesh);
-      if(mesh.type==='Mesh'){
-        //Mesh,Object3D
-        let axes=new THREE.AxesHelper(3);
-        // axes.material.depthTest=false;
-        mesh.add(axes);
-      }else{
-        let grid=new THREE.GridHelper(10,10);
-        mesh.add(grid);
-      }
-
-      
-
-    });
-    //最后我们将root添加到场景中。
-    f.scene.add(f.mesh[0]);
+    this.createCubes();
   }
   //MESH: rotation,position,scale
   operateMeshInRender(timeSec){
-    this.mesh.forEach(function(mesh,i){
+    /*this.mesh.forEach(function(mesh,i){
       // mesh.rotation.y=timeSec/2;
-    });
+    });*/
+    this.cameraPole.rotation.y=timeSec/40;
   }
   //We also need to go to each mesh in the scene and decide if it should both cast shadows and/or receive shadows.
-  createPlane(){
-    let width=30;
-    let geometry=new THREE.PlaneGeometry(width,width);
-    // let geometry=new THREE.WireframeGeometry(box_geometry);
 
-
-    let loader=new THREE.TextureLoader();
-    let texture=loader.load('./resources/images/checker2_2.png');
-    // console.log(texture);
-    texture.wrapS=THREE.RepeatWrapping;
-    texture.wrapT=THREE.RepeatWrapping;
-    texture.magFilter=THREE.NearestFilter;
-    let repeats=width/2;
-    texture.repeat.set(repeats,repeats);
-    let material=new THREE.MeshPhongMaterial({
-      map:texture,
-      side:THREE.DoubleSide
-    });
-
-    let mesh=new THREE.Mesh(geometry,material);
-    mesh.rotation.x=-Math.PI/2;
-    mesh.receiveShadow=true;
-    return (mesh);
-  }
-  createCube(color='floralwhite',scale=1){
-    let width=4;
+  createCubes(){
+    let width=1;
     let geometry=new THREE.BoxGeometry(width,width,width);
-    let material=new THREE.MeshPhongMaterial({
-      color:color
-    });
-    let mesh=new THREE.Mesh(geometry,material);
-    mesh.scale.set(scale,scale,scale);
-    mesh.position.set(width+1,width/2,0);
-    // mesh.rotation.y=-.4;
-    mesh.castShadow=true;
-    mesh.receiveShadow=true;
-    return (mesh);
-  }
-  createSphere(color='crimson',scale=1){
-    let radius=3;
-    let geometry=new THREE.SphereGeometry(radius,50,50);
-    let material=new THREE.MeshPhongMaterial({
-      // flatShading:true,
-      color:color
-    });
-    let mesh=new THREE.Mesh(geometry,material);
-    mesh.scale.set(scale,scale,scale);
-    mesh.position.set(-radius-1,radius+2,0);
-    mesh.castShadow=true;
-    mesh.receiveShadow=true;
-    return (mesh);
+    
+    var arrCubes=[];
+
+    for(var i=0;i<this.cubeNums;i++){
+      let material=new THREE.MeshPhongMaterial({
+        color:this.randHSL()
+      });
+      let mesh=new THREE.Mesh(geometry,material);
+
+      this.scene.add(mesh);
+      mesh.scale.set(this.rand(3,6),this.rand(3,6),this.rand(3,6));
+      mesh.position.set(this.rand(-30,30),this.rand(-30,30),this.rand(-30,30));
+      mesh.rotation.set(this.rand(Math.PI),this.rand(Math.PI),0);
+      mesh.castShadow=true;
+      mesh.receiveShadow=true;
+    }
+    
+    
   }
 
   //灯光
@@ -251,10 +178,10 @@ class O_xyz {
     this.light.position.set(1,5,9);
     //Then we also need to tell the light to cast a shadow
     this.light.castShadow=true;
-    let lightHelper=new THREE.PointLightHelper(this.light);
+    // let lightHelper=new THREE.PointLightHelper(this.light);
 
-    this.scene.add(this.light);
-    this.scene.add(lightHelper);
+    this.camera.add(this.light);
+    // this.camera.add(lightHelper);
   }
 
   //gui
@@ -262,32 +189,51 @@ class O_xyz {
     let gui=new GUI();
     gui.add(this.light,'intensity',0,2,0.001);
   }
+  rand(min,max){
+    if(max===undefined){
+      max=min;
+      min=0;
+    }
+    return (min+(max-min)*Math.random());
+  }
+
+  randHSL(){
+    return (`hsl(${this.rand(360) | 0},${this.rand(50,100) | 0}%,50%)`);
+  }
 
 
-  getCanvasRelativePosition(ev){
-    var f=this;
-    var rect=f.canvas.getBoundingClientRect();
-    return ({
-      x:(ev.clientX-rect.left)*f.canvas.width/rect.width,
-      y:(ev.clientY-rect.top)*f.canvas.height/rect.height
-    });
-    return f;
-  }
-  setPickPosition(ev){
-    var f=this;
-    var pos=f.getCanvasRelativePosition(ev);
-    f.pickPosition.x=(pos.x/this.canvas.width)*2-1
-    f.pickPosition.y=-((pos.y/this.canvas.height)*2-1)
-    // console.log(f.pickPosition);
-    return f;
-  }
-  //拣物件
-  pickEvent(){
+  addPickEvent(){
     var f=this;
     window.addEventListener('mousemove',function(ev){
-      f.setPickPosition(ev);
+      var pos={
+        x:ev.clientX,
+        y:ev.clientY
+      };
+
+      var w=f.canvas.width;
+      var h=f.canvas.height;
+      f.pickPosition={
+        x:2*pos.x/w-1,
+        y:-(2*pos.y/h-1)
+      };
     });
-    return f;
+
+    var clearPickPosition=function(){
+      f.pickPosition={
+        x:-1e6,
+        y:-1e6
+      };
+    };
+
+
+    clearPickPosition();
+
+    window.addEventListener('mouseout',function(){
+      clearPickPosition();
+    });
+    window.addEventListener('mouseleave',function(){
+      clearPickPosition();
+    });
   }
 
 
@@ -295,5 +241,7 @@ class O_xyz {
 }
 
 window.pickHelper=new PickHelper();
+
+
 window.obj=new O_xyz();
 window.obj.init();
