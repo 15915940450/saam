baffad5138e0bd906b786cabd9eb4d371dfedec0
15915940450 <15915940450@139.com>
2020-07-06 17:47:14 +0800

    earth sphere

--- test.js
+++ test.js
@@ -1,306 +1,149 @@
 import * as THREE from './__3/three.module.js';
 import {OrbitControls} from './__3/OrbitControls.js';
-import {GUI} from './__3/dat.gui.module.js';
 
-window.__3=THREE;
+function main() {
+  const canvas = document.querySelector('#c');
+  const renderer = new THREE.WebGLRenderer({canvas});
 
-class O_xyz {
-  constructor(){
-    this.canvas=document.querySelector('#c');
-    this.elLabel=document.querySelector('#labels');
-    this.meshs=[];
-  }
-  init(){
-    this.createRenderer();
-    this.createScene();
-    this.createAcamera();
-    this.createControls();
-    this.addMesh();
-    this.addLight();
-    this.gui();
+  const fov = 60;
+  const aspect = 2;  // the canvas default
+  const near = 0.1;
+  const far = 10;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 2.5;
 
-    this.raf();
-  }
+  const controls = new OrbitControls(camera, canvas);
+  controls.enableDamping = true;
+  controls.enablePan = false;
+  controls.minDistance = 1.2;
+  controls.maxDistance = 4;
+  controls.update();
 
-  raf(){
-    var f=this;
-    var i=0;
-    //raf
-    let rafCallback=function(time){
-      i++;
-      time=time/1000;
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color('#236');
 
-      //操作物件得以渲染出变化(运动)
-      f.operateMeshInRender(time,i);
-      
-      
-      //camera aspect
-      if(f.resizeRenderer2DisplaySize(f.renderer)){
-        f.camera.aspect=f.canvas.clientWidth/f.canvas.clientHeight;
-        f.camera.updateProjectionMatrix();
-      }
-
-      f.renderer.render(f.scene,f.camera);
-
-      window.requestAnimationFrame(rafCallback);
-    };
-    window.requestAnimationFrame(rafCallback);
-    return f;
+  {
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load('./resources/images/country-outlines-4k.png', render);
+    const geometry = new THREE.SphereBufferGeometry(1, 64, 32);
+    const material = new THREE.MeshBasicMaterial({map: texture});
+    scene.add(new THREE.Mesh(geometry, material));
   }
 
-  //===创建渲染器
-  createRenderer(){
-    let f=this;
-    f.renderer=new THREE.WebGLRenderer({
-      canvas:this.canvas
-    });
-
-    //The first thing we need to do is turn on shadows in the renderer.
-    f.renderer.shadowMap.enabled=true;
+  async function loadJSON(url) {
+    const req = await fetch(url);
+    return req.json();
   }
 
+  let countryInfos;
+  async function loadCountryData() {
+    countryInfos = await loadJSON('./resources/country-info.json');  
 
-  //场景
-  createScene(){
-    this.scene=new THREE.Scene();
-    this.scene.background=new THREE.Color(0x333333);
+    const lonFudge = Math.PI * 1.5;
+    const latFudge = Math.PI;
+    // these helpers will make it easy to position the boxes
+    // We can rotate the lon helper on its Y axis to the longitude
+    const lonHelper = new THREE.Object3D();
+    // We rotate the latHelper on its X axis to the latitude
+    const latHelper = new THREE.Object3D();
+    lonHelper.add(latHelper);
+    // The position helper moves the object to the edge of the sphere
+    const positionHelper = new THREE.Object3D();
+    positionHelper.position.z = 1;
+    latHelper.add(positionHelper);
+
+    const labelParentElem = document.querySelector('#labels');
+    for (const countryInfo of countryInfos) {
+      const {lat, lon, name} = countryInfo;
+
+      // adjust the helpers to point to the latitude and longitude
+      lonHelper.rotation.y = THREE.MathUtils.degToRad(lon) + lonFudge;
+      latHelper.rotation.x = THREE.MathUtils.degToRad(lat) + latFudge;
+
+      // get the position of the lat/lon
+      positionHelper.updateWorldMatrix(true, false);
+      const position = new THREE.Vector3();
+      positionHelper.getWorldPosition(position);
+      countryInfo.position = position;
+
+      // add an element for each country
+      const elem = document.createElement('div');
+      elem.textContent = name;
+      labelParentElem.appendChild(elem);
+      countryInfo.elem = elem;
+    }
+    requestRenderIfNotRequested();
   }
+  loadCountryData();
 
-  //相机
-  createAcamera(){
-    let fov=45;
-    let aspect=2; //默认是2
-    let near=0.1;
-    let far=100;
-    //摄像机默认指向Z轴负方向，上方向朝向Y轴正方向。
-    this.camera=new THREE.PerspectiveCamera(fov,aspect,near,far);
+  const tempV = new THREE.Vector3();
 
-    this.camera.position.set(5,10,30);
-    // this.camera.up.set(0,0,1);
-    // this.camera.lookAt(0,0,0);
-  }
-  //环绕控制
-  createControls(){
-    this.controls=new OrbitControls(this.camera,this.canvas);
-    this.controls.target.set(0,4,0);
-    this.controls.update();
-  }
-
-
-  //物件
-  addMesh(){
-    let f=this;
-
-
-    //rootObject3D
-    let rootObject3D=new THREE.Object3D();
-    this.meshs.push(rootObject3D);
-
-
-    let plane=this.createPlane();
-    rootObject3D.add(plane);
-    
-    this.targetCube=this.createCube('方体');
-    let cube=this.targetCube.mesh;
-    rootObject3D.add(cube);
-
-    this.targetSphere=this.createSphere('波-球');
-    let sphere=this.targetSphere.mesh;
-    rootObject3D.add(sphere);
-    
-    
-    //最后我们将root添加到场景中。
-    f.scene.add(f.meshs[0]);
-  }
-  
-  //We also need to go to each mesh in the scene and decide if it should both cast shadows and/or receive shadows.
-  createPlane(){
-    let width=30;
-    let geometry=new THREE.PlaneGeometry(width,width);
-    // let geometry=new THREE.WireframeGeometry(box_geometry);
-
-
-    let loader=new THREE.TextureLoader();
-    let texture=loader.load('./resources/images/checker2_2.png');
-    // console.log(texture);
-    texture.wrapS=THREE.RepeatWrapping;
-    texture.wrapT=THREE.RepeatWrapping;
-    texture.magFilter=THREE.NearestFilter;
-    let repeats=width/2;
-    texture.repeat.set(repeats,repeats);
-    let material=new THREE.MeshPhongMaterial({
-      map:texture,
-      side:THREE.DoubleSide
-    });
-
-    let mesh=new THREE.Mesh(geometry,material);
-    mesh.rotation.x=-Math.PI/2;
-    mesh.receiveShadow=true;
-
-    // this.meshs.push(mesh);
-    return (mesh);
-  }
-  createCube(name,color='floralwhite',scale=1){
-    let width=4;
-    let geometry=new THREE.BoxGeometry(width,width,width);
-    let material=new THREE.MeshPhongMaterial({
-      emissive:0x309bff,
-      color:color
-    });
-    let mesh=new THREE.Mesh(geometry,material);
-
-    mesh.name=name;
-    mesh.scale.set(scale,scale,scale);
-    mesh.position.set(width+1,width/2,-15);
-    // mesh.rotation.y=-.4;
-    mesh.castShadow=true;
-    mesh.receiveShadow=true;
-    this.meshs.push(mesh);
-
-    var elem=document.createElement('div');
-    elem.textContent=name;
-    this.elLabel.appendChild(elem);
-
-    return ({mesh,elem});
-  }
-  createSphere(name,color='crimson',scale=1){
-    let radius=3;
-    let geometry=new THREE.SphereGeometry(radius,50,50);
-    let material=new THREE.MeshPhongMaterial({
-      flatShading:true,
-      color:color
-    });
-    let mesh=new THREE.Mesh(geometry,material);
-
-    mesh.name=name;
-
-    mesh.scale.set(scale,scale,scale);
-    mesh.position.set(-radius-1,radius+2,0);
-    mesh.castShadow=true;
-    mesh.receiveShadow=true;
-    this.meshs.push(mesh);
-
-
-    var elem=document.createElement('div');
-    elem.textContent=name;
-    this.elLabel.appendChild(elem);
-
-    return ({mesh,elem});
-  }
-
-  //灯光
-  addLight(){
-    let intensity=.951;
-    this.light=new THREE.PointLight(0xffffff,intensity);
-    this.light.position.set(1,5,9);
-    //Then we also need to tell the light to cast a shadow
-    this.light.castShadow=true;
-    let lightHelper=new THREE.PointLightHelper(this.light);
-
-    this.scene.add(this.light);
-    this.scene.add(lightHelper);
-  }
-
-  //gui
-  gui(){
-    let gui=new GUI();
-    gui.add(this.light,'intensity',0,2,0.001);
-  }
-
-  //set canvas drawingbuffer
-  resizeRenderer2DisplaySize(){
-    var f=this;
-    let needResize=true;
-    let targetWidth=this.canvas.clientWidth*window.devicePixelRatio | 0;
-    let targetHeight=this.canvas.clientHeight*window.devicePixelRatio | 0;
-
-    if(targetWidth===this.canvas.width && targetHeight===this.canvas.height){
-      needResize=false;
+  function updateLabels() {
+    // exit if we have not yet loaded the JSON file
+    if (!countryInfos) {
+      return;
     }
 
-    if(needResize){
-      f.renderer.setSize(targetWidth,targetHeight,false);
-    }
+    for (const countryInfo of countryInfos) {
+      const {position, elem} = countryInfo;
 
-    return (needResize);
+      // get the normalized screen coordinate of that position
+      // x and y will be in the -1 to +1 range with x = -1 being
+      // on the left and y = -1 being on the bottom
+      tempV.copy(position);
+      tempV.project(camera);
+
+      // convert the normalized position to CSS coordinates
+      const x = (tempV.x *  .5 + .5) * canvas.clientWidth;
+      const y = (tempV.y * -.5 + .5) * canvas.clientHeight;
+
+      // move the elem to that position
+      elem.style.transform = `translate(-50%, -50%) translate(${x}px,${y}px)`;
+
+      // set the zIndex for sorting
+      elem.style.zIndex = (-tempV.z * .5 + .5) * 100000 | 0;
+    }
   }
 
-  //operate MESH: rotation,position,scale
-  operateMeshInRender(timeSec,i){
-    this.meshs.forEach(function(mesh,i){
-      // mesh.rotation.y=timeSec/4;
-    });
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
 
+  let renderRequested = false;
 
-    {
-      // console.log(this.targetCube);
-      let {mesh,elem}=this.targetCube;
-      // mesh.position.y=timeSec/10;
-      // mesh.rotation.x=timeSec/3;
-      // mesh.updateWorldMatrix(true,false);
+  function render() {
+    renderRequested = undefined;
 
-
-      //1.获得世界坐标 get the position of the center of the cube
-      let kelefeV=new THREE.Vector3();
-      mesh.getWorldPosition(kelefeV);
-      //2.世界坐标转标准设备坐标(经过相机变换后的坐标)
-      /*
-      worldVector.project(camera);返回的结果是世界坐标worldVector在camera相机对象矩阵变化下对应的标准设备坐标， 标准设备坐标xyz的范围是[-1,1]
-      */
-      kelefeV.project(this.camera);
-
-      //算出荧幕坐标值(px)
-      /*let x=(this.kelefeV.x)*0.5*this.canvas.clientWidth+this.canvas.clientWidth*0.5;
-      let y=-(this.kelefeV.y)*0.5*this.canvas.clientHeight+this.canvas.clientHeight*0.5;*/
-      /*Xpx=width(x+1)/2;
-      Ypx=-height(y-1)/2;*/
-      let Xpx=this.canvas.clientWidth*(kelefeV.x+1)/2;
-      let Ypx=-this.canvas.clientHeight*(kelefeV.y-1)/2;
-
-      elem.style.transform=`
-        translate(-50%,-50%) translate(${Xpx}px,${Ypx}px)
-      `;
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
     }
 
-    {
-      let {mesh,elem}=this.targetSphere;
+    controls.update();
 
-      let kelefeV=new THREE.Vector3();
+    updateLabels();
 
-      mesh.getWorldPosition(kelefeV);
-      kelefeV.project(this.camera);
+    renderer.render(scene, camera);
+  }
+  render();
 
-      let x=(kelefeV.x)*0.5*this.canvas.clientWidth+this.canvas.clientWidth*0.5;
-      let y=-(kelefeV.y)*0.5*this.canvas.clientHeight+this.canvas.clientHeight*0.5;
-
-      elem.style.transform=`
-        translate(-50%,-50%) translate(${x}px,${y}px)
-      `;
+  function requestRenderIfNotRequested() {
+    if (!renderRequested) {
+      renderRequested = true;
+      requestAnimationFrame(render);
     }
+  }
 
+  controls.addEventListener('change', requestRenderIfNotRequested);
+  window.addEventListener('resize', requestRenderIfNotRequested);
+}
 
-    //==============首次进入，一次性
-    if(i===1){
-      //添加辅助axes,grid
-      this.meshs.forEach(function(mesh,i){
-
-        // console.log(mesh);
-
-        if(mesh.type==='Mesh'){
-          //Mesh,Object3D
-          let axes=new THREE.AxesHelper(3);
-          // axes.material.depthTest=false;
-          mesh.add(axes);
-        }else{
-          let grid=new THREE.GridHelper(10,10);
-          mesh.add(grid);
-        }
-      });
-    } //end if
-  } //functions end width operateMeshInRender
-} //end class
-
-
-window.obj=new O_xyz();
-window.obj.init();
-
+main();
