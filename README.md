7a1d4a888899a01f435ee216ce488379bc9ba642
15915940450 <15915940450@139.com>
2020-07-10 10:47:19 +0800

    promise, then

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 three.js
 
 + sublime snippet fold ==> C:\Users\ili\AppData\Roaming\Sublime Text 3\Packages\User
+
+
+http://cilibao.biz/
+http://www.ciliba.fun/
+
+magnet:?xt=urn:btih:8E13DC7C1451C44FB102482728460CC965D3CE13
+magnet:?xt=urn:btih:41117E2F7D24C9F5BC4BD2F55A65B2B4BAFB0F01
+magnet:?xt=urn:btih:3fceb773e23c0f55c981b2a30e1685255f05cece
--- test.js
+++ test.js
@@ -13,14 +13,14 @@ function main() {
   camera.position.z = 2.5;
 
   const controls = new OrbitControls(camera, canvas);
-  controls.enableDamping = true;
-  controls.enablePan = false;
-  controls.minDistance = 1.2;
-  controls.maxDistance = 4;
+  // controls.enableDamping = true;
+  // controls.enablePan = false;
+  // controls.minDistance = 1.2;
+  // controls.maxDistance = 4;
   controls.update();
 
   const scene = new THREE.Scene();
-  scene.background = new THREE.Color('#236');
+  scene.background = new THREE.Color('#d50000');
 
   {
     const loader = new THREE.TextureLoader();
@@ -32,12 +32,56 @@ function main() {
 
   async function loadJSON(url) {
     const req = await fetch(url);
-    return req.json();
+    // console.log(req);
+    var reqjson=req.json();
+    // console.log(reqjson);
+    return reqjson;
   }
+
+
+  //============================================================================fetch
+  var tttoken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySW5mbyI6IntcImlkXCI6XCI2MkVCREUwMEEzRDQ0MTE4QjVEMTEzMzU5QkYxM0IyM1wiLFwicGhvbmVcIjpcIjE5OTY2NjY5OTk5XCIsXCJuYW1lXCI6XCJpbGlcIixcInJvbGVzXCI6W3tcImlkXCI6XCI0Y2Y0OWYyNjExMTM0NTdkYmVlOWFmYzg2ZWMwNWU1ZFwiLFwibmFtZVwiOlwic3VwZXJhZG1pblwiLFwiZW5hYmxlZFwiOjEsXCJ0aW1lXCI6MTUwMTgyNzMxNjAwMCxcImRlc2NyaXB0aW9uXCI6XCLotoXnuqfnrqHnkIblkZgxMjNcIixcInBsYXRmb3JtXCI6MSxcImVkaXRcIjoxfV19IiwidGltZSI6MTU5NDM0NTc3NzAzOSwiZXhwIjoxNTk0MzUyOTc3fQ.Jq7qQcLHALJCIn1HgYH5BbgUQZyza7uFF8wIyn4JHbc"
+  async function rqs(){
+    var __url='http://admin.ce59e336fb1da4a72a0f53e21dd1598a6.cn-shenzhen.alicontainer.com/ecadmin/statistics/total';
+    var x=await fetch(__url,{
+      headers:{
+        AccessToken:tttoken
+      }
+    });
+    var json=x.json();
+    console.log(json);
+    console.log(typeof json);
+    json.then(function(aaa){
+      console.log(aaa);
+    });
+  };
+  rqs();
+  /*var __url='http://ecadmin.ce59e336fb1da4a72a0f53e21dd1598a6.cn-shenzhen.alicontainer.com/ecadmin/statistics/total';
+  fetch(__url).then(function(res){
+    return (res.json());
+  }).then(function(json){
+    console.log(json);
+  });*/
+  //============================================================================
+
+  /*
+  //上述代碼的寫法就是具有適當錯誤處理的簡單明確的鍊式寫法。
+
+  doSomething()
+  .then(function(result) {
+    return doSomethingElse(result);
+  })
+  .then(newResult => doThirdThing(newResult))
+  .then(() => doFourthThing())
+  .catch(error => console.log(error));
+
+  //使用async/await可以解決以上大多數錯誤，使用async/await時，最常見的語法錯誤就是忘記了await關鍵字。
+  */
 
   let countryInfos;
   async function loadCountryData() {
-    countryInfos = await loadJSON('./resources/country-info.json');  
+    countryInfos = await loadJSON('./resources/country-info.json');
+    // console.log(countryInfos);
 
     const lonFudge = Math.PI * 1.5;
     const latFudge = Math.PI;
@@ -147,8 +191,3 @@ function main() {
 }
 
 main();
-
-
-// http://cilibao.biz/
-// http://www.ciliba.fun/
-
